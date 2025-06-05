8f8c5c0d91099620c3c324feb215dd4352bf453f
Dannygnr94x <lukas.mune@gmail.com>
2025-06-06 00:12:18 +0200

    Subiendo el archivo 03

---
+++ 03-exercises.js
@@ -0,0 +1,42 @@
+// 1. Escribe un comnt en una linea
+
+// Escribo un comnt en una linea
+
+// 2. Escrive un comnt en varias lineas
+
+/*
+Escribo un comnt
+en varias
+lineas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivo
+
+let myName = "Daniel"
+let myAge = 30
+let ImVirgil = true
+let undefinedValue
+let nullvalue = null
+let mySymbol = Symbol("mysymbol")
+let myBigInt = 83474863645873648639847509375926893740229346n
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(myName)
+console.log(myAge)
+console.log(ImVirgil)
+console.log(undefinedValue)
+console.log(nullvalue)
+console.log(mySymbol)
+console.log(myBigInt)
+
+// 5. imprime por consola el tipo de todas las variables
+
+console.log(typeof myName)
+console.log(typeof myAge)
+console.log(typeof ImVirgil)
+console.log(typeof undefinedValue)
+console.log(typeof nullvalue)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt2)
+
